4bf43fa3cc3dbca613e395348f6f2a30d3f3b65f
Omid Ashtari <omid@Omids-Mac-mini-2.local>
2019-11-24 19:58:38 -0800

    added answers in correct file, got pagination to work

--- starwars/src/components/SwapiPerson/SwapiPersonList.js
+++ starwars/src/components/SwapiPerson/SwapiPersonList.js
@@ -1,25 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import StarWarsCard from './SwapiPersonCard';
-import { Container, Row } from 'reactstrap';
+import { Container, Row, Button } from 'reactstrap';
+
+let count = 1;
 
 export default function StarWarsPerson() {
     const [person, setPerson] = useState([]);
     const [people, setPeople] = useState([]);
+    const [endpoint, setEndpoint] = useState(`https://swapi.co/api/people/?page=${count}`);
 
+    function NextPage(event) {
+        event.preventDefault();
+        count++;
+        if (count < 10) {
+            setEndpoint(`https://swapi.co/api/people/?page=${count}`);
+        } else {
+            count--;
+            setEndpoint(`https://swapi.co/api/people/?page=${count}`);
+        }
+    }
+
+    function PrevPage(event) {
+        event.preventDefault();
+        count--;
+        if (count > 0) {
+            setEndpoint(`https://swapi.co/api/people/?page=${count}`);
+        } else {
+            count++;
+            setEndpoint(`https://swapi.co/api/people/?page=${count}`);
+        }
+    }
 
     useEffect(() => {
         axios
-            .get(`https://swapi.co/api/people`)
+            .get(endpoint)
             .then(response => {
                 console.log(response);
                 setPerson(response.data.results);
             })
             .catch(error => console.log(error))
-    }, []);
+    }, [count]);
 
     return (
         <Container>
+            <Button onClick={PrevPage} style={{ margin: '5px', backgroundColor: '#444545' }}>PREVIOUS PAGE</Button>
+            <Button onClick={NextPage} style={{ margin: '5px', backgroundColor: '#444545' }}>NEXT PAGE</Button>
             <Row>
                 {person.map(character => {
                     return <StarWarsCard character={character} key={character.url} />
