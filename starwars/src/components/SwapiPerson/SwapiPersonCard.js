18896622e6a24a34e91824099a8c720c9e54f25f
Omid Ashtari <omid@Omids-Mac-mini-2.local>
2019-11-24 19:02:12 -0800

    pulling and displaying data, cards have been styled

--- starwars/src/components/SwapiPerson/SwapiPersonCard.js
+++ starwars/src/components/SwapiPerson/SwapiPersonCard.js
@@ -3,15 +3,15 @@ import { Card, CardHeader, CardBody, CardText, Col, Button } from 'reactstrap';
 
 const StarWarsCard = ({ character }) => {
     return (
-        <Col xs='6' md='4'>
-            <Card>
-                <CardHeader>Name: {character.name}</CardHeader>
-                <CardBody>
+        <Col md='6'>
+            <Card style={{ margin: '20px', border: "3px dotted #CEABB1" }}>
+                <CardHeader style={{ margin: '5px', backgroundColor: '#C5E0D8' }}>Name: {character.name}</CardHeader>
+                <CardBody style={{ backgroundColor: '#F6DF9E' }}>
                     <CardText>Year of Birth: {character.birth_year}</CardText>
                     <CardText>Color of Hair: {character.hair_color}</CardText>
                 </CardBody>
             </Card>
-        </Col>
+        </Col >
     )
 }
 
